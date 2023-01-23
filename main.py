051fc41c128222cba66c225d5b19bb5fca183ffa
max <maksnasikoaskij@gmail.com>
2023-01-23 14:57:57 +0200

    add background image

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import os
 pygame.init()
 
-def path_file(file_name):
+def file_path (file_name):
     folder = os.path.abspath(__file__ + "/..")
     path = os.path.join(folder, file_name)
     return path
@@ -10,6 +10,9 @@ def path_file(file_name):
 
 WIN_WIDTH, WIN_HEIGHT = 700, 500
 FPS = 40
+
+image_background = pygame.image.load(file_path("images (1).jfif"))
+image_background = pygame.transform.scale(image_background, (WIN_WIDTH, WIN_HEIGHT))
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Шутер")
@@ -22,6 +25,9 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+
+    if play == True:
+        window.blit(image_background,(0, 0))
 
     clock.tick(FPS)
     pygame.display.update()
